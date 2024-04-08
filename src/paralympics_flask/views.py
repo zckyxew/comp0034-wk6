6735859fa44b1720bb48ca53a18b9a3d0c1d246c
EthanWoolley <30636974+EthanWoolley@users.noreply.github.com>
2024-04-08 18:29:35 +0100

    Add Flask and Dash dependencies, create index.html template, update views.py

--- src/paralympics_flask/views.py
+++ src/paralympics_flask/views.py
@@ -1,9 +1,9 @@
 from flask import current_app as app
-
+from flask import render_template
 
 @app.route('/', methods=['GET'])
 def index():
     """
-    Returns a view (HTML page) with a greeting message 'Hello, world'.
+    Returns a HTML page, based on the template.
     """
-    return 'Hello, world!'
+    return render_template('index.html')
